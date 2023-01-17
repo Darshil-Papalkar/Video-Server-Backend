7f4362e4eb165872d3668ad6d085c99873a0c7ce
Webpage-builder <papalkardarshil12@gmail.com>
2023-01-17 20:15:53 +0530

    files severed using readStream piping

--- app.js
+++ app.js
@@ -5,14 +5,60 @@ const express = require("express");
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  next();
+});
+
+app.get("/", (req, res, next) => {
+  res.send("Hello From Server");
+});
+
 app.get("/lover", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "assets", "lover.mp4"));
-})
+  const range = req.headers.range;
+  const videoPath = path.join(__dirname, "assets", "lover.mp4");
+  const videoSize = fs.statSync(videoPath).size;
+  console.log(range);
+  if (!range) {
+    const head = {
+      "Content-Length": videoSize,
+      "Content-Type": "video/mp4",
+    };
+    res.writeHead(200, head);
+    fs.createReadStream(videoPath).pipe(res);
+  } else {
+    const parts = range.replace(/bytes=/, "").split("-");
+    console.log(parts);
+    const start = parseInt(parts[0], 10);
+    const end = parts[1] ? parseInt(parts[1], 10) : videoSize - 1;
+    const chunkSize = end - start + 1;
+
+    const headers = {
+      "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+      "Accept-Ranges": "bytes",
+      "Content-Length": chunkSize,
+      "Content-Type": "video/mp4",
+    };
+    res.writeHead(206, headers);
+
+    const videoStream = fs.createReadStream(videoPath, { start, end });
+    videoStream.pipe(res);
+  }
+});
 
 app.get("/vedLavly", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "assets", "vedLavly.mp4"));
-})
+  const range = req.headers.range;
+  if (!range) {
+    return res.status(400).send("Requires Range header");
+  }
+  res.sendFile(path.join(__dirname, "assets", "uuchai.mp4"));
+});
 
-app.listen(8080, () => {
+app.listen(5000, () => {
   console.log("Backend Server Started!");
 });
